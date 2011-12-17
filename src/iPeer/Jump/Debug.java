a6543136210fa1ab1b46d91384480f83f553a5b3
iPeer <ipeer@ipeerftw.co.cc>
2011-12-17 03:11:11 +0000

    Initial Code

---
+++ src/iPeer/Jump/Debug.java
@@ -0,0 +1,18 @@
+package iPeer.Jump;
+
+public class Debug {
+
+	public static void p(boolean b) {
+		System.out.println(b);
+	}
+
+	public static void p(String s) {
+		System.out.println(s);
+	}
+
+	public static void p(int i) {
+		System.out.println(i);
+		
+	}
+
+}
---
+++ src/iPeer/Jump/KeyInputHandler.java
@@ -0,0 +1,39 @@
+package iPeer.Jump;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyInputHandler implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		lastKey = e.getKeyCode();
+		Debug.p("Key Pressed: "+e.getKeyCode());
+		if (lastKey == 114) {
+			Game.debugActive = !Game.debugActive;
+		}
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		lastKey = e.getKeyCode();
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		lastKey = e.getKeyChar();
+		if (e.getKeyChar() == 27) {
+			Debug.p("Exit!");
+			System.exit(0);
+		}
+
+	}
+	
+	public static int getLastKey() {
+		return lastKey;
+	}
+	public static int lastKey;
+
+}
